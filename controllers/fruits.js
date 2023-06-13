e39dc438928fec4b21f1ce5c6011c6fc88b42c28
Alex Merced <alex@alexmerced.dev>
2023-06-12 22:21:16 -0400

    update

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -12,13 +12,13 @@ const router = express.Router()
 // INDEX - GET - LIST ALL FRUITS - /fruit
 router.get("/", (req, res) => {
     // render an ejs template with all the fruits
-    res.render("index.ejs", {fruits})
+    res.render("fruits/index.ejs", {fruits})
   })
   
 // NEW - GET - SHOW A FORM TO CREATE A FRUIT
 router.get("/new", (req, res) => {
     // render the new template
-    res.render("new.ejs")
+    res.render("fruits/new.ejs")
   })
   
 // DESTROY - DELETE - DELETE A FRUIT
@@ -62,7 +62,7 @@ router.get("/:id/edit", (req, res) => {
     // get the fruit using the index
     const fruit = fruits[id]
     // render the template, pass the fruit and index
-    res.render("edit.ejs", {fruit, id})
+    res.render("fruits/edit.ejs", {fruit, id})
   })
   
   
@@ -75,7 +75,7 @@ router.get("/:id", (req, res) => {
       // dynamically set a class
       const readyClass = fruit.readyToEat ? "green" : "red"
       // render a template with the fruit
-      res.render("show.ejs", {fruit, readyClass, id})
+      res.render("fruits/show.ejs", {fruit, readyClass, id})
   })
 
 
